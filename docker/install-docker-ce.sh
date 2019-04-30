345a7b0efbd067a3ca5bae65e1171249dfe0a1a7
DominiqN <Nam.Dongmin@htbeyond.com>
2019-04-30 19:32:22 +0900

    Add docker installing script and genesis.json

---
+++ docker/install-docker-ce.sh
@@ -0,0 +1,33 @@
+# 2019-04-30
+
+sudo apt-get update
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+# if arch is different, change arch.
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# 사용자 이름이 달라지면 고쳐야 함
+echo "htbeyond"
+
+sudo usermod -aG docker htbeyond
+
+echo "logout please"
