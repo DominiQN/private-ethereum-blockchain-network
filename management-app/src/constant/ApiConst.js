076da4a187fd0a5eac1ec6f5df22c38a9e0f688f
DominiQN <dmut7691@gmail.com>
2019-06-17 04:17:28 +0900

    Update abi

--- management-app/src/constant/ApiConst.js
+++ management-app/src/constant/ApiConst.js
@@ -3,11 +3,111 @@ const abi = [
 		"constant": false,
 		"inputs": [
 			{
+				"name": "ip",
+				"type": "uint32"
+			},
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "status",
+				"type": "string"
+			}
+		],
+		"name": "createFacility",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
 				"name": "addr",
 				"type": "address"
 			}
 		],
-		"name": "getCard",
+		"name": "getCardAuth",
+		"outputs": [
+			{
+				"name": "ips",
+				"type": "uint32[]"
+			},
+			{
+				"name": "names",
+				"type": "string[]"
+			},
+			{
+				"name": "size",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getCardSize",
+		"outputs": [
+			{
+				"name": "cardSize",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "addr",
+				"type": "address"
+			},
+			{
+				"name": "auth",
+				"type": "uint32[]"
+			}
+		],
+		"name": "updateCardAuth",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
@@ -30,35 +130,296 @@ const abi = [
 			},
 			{
 				"name": "dong",
-				"type": "uint32"
+				"type": "string"
 			},
 			{
 				"name": "ho",
+				"type": "string"
+			}
+		],
+		"name": "createCard",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "givenIp",
 				"type": "uint32"
 			}
 		],
-		"name": "setCard",
-		"outputs": [],
+		"name": "getFacilityInfo",
+		"outputs": [
+			{
+				"name": "ip",
+				"type": "uint32"
+			},
+			{
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"name": "status",
+				"type": "string"
+			}
+		],
 		"payable": false,
-		"stateMutability": "nonpayable",
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "tokenName",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "givenAddr",
+				"type": "address"
+			}
+		],
+		"name": "getCardInfo",
+		"outputs": [
+			{
+				"name": "addr",
+				"type": "address"
+			},
+			{
+				"name": "id",
+				"type": "string"
+			},
+			{
+				"name": "status",
+				"type": "string"
+			},
+			{
+				"name": "dong",
+				"type": "string"
+			},
+			{
+				"name": "ho",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "size",
+				"type": "uint256"
+			},
+			{
+				"name": "page",
+				"type": "uint256"
+			}
+		],
+		"name": "getCardList",
+		"outputs": [
+			{
+				"name": "ids",
+				"type": "string[]"
+			},
+			{
+				"name": "statuses",
+				"type": "string[]"
+			},
+			{
+				"name": "dongs",
+				"type": "string[]"
+			},
+			{
+				"name": "hos",
+				"type": "string[]"
+			},
+			{
+				"name": "lastPage",
+				"type": "uint256"
+			},
+			{
+				"name": "currentPageSize",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getFacilitySize",
+		"outputs": [
+			{
+				"name": "cardSize",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
 		"constant": false,
 		"inputs": [
 			{
-				"name": "_to",
+				"name": "addr",
 				"type": "address"
-			},
-			{
-				"name": "_value",
-				"type": "uint256"
 			}
 		],
-		"name": "transfer",
+		"name": "clearAuth",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getFacilityKeys",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint32[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "addr",
+				"type": "address"
+			},
+			{
+				"name": "facilityIp",
+				"type": "uint32"
+			},
+			{
+				"name": "accessTimestamp",
+				"type": "uint256"
+			}
+		],
+		"name": "access",
+		"outputs": [
+			{
+				"name": "authorized",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "size",
+				"type": "uint256"
+			},
+			{
+				"name": "page",
+				"type": "uint256"
+			}
+		],
+		"name": "getFacilityList",
+		"outputs": [
+			{
+				"name": "ips",
+				"type": "uint32[]"
+			},
+			{
+				"name": "names",
+				"type": "string[]"
+			},
+			{
+				"name": "statuses",
+				"type": "string[]"
+			},
+			{
+				"name": "lastPage",
+				"type": "uint256"
+			},
+			{
+				"name": "currentPageSize",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getCardKeys",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
@@ -105,15 +466,15 @@ const abi = [
 			{
 				"indexed": false,
 				"name": "dong",
-				"type": "uint32"
+				"type": "string"
 			},
 			{
 				"indexed": false,
 				"name": "ho",
-				"type": "uint32"
+				"type": "string"
 			}
 		],
-		"name": "getCardEvent",
+		"name": "SetCard",
 		"type": "event"
 	},
 	{
@@ -121,183 +482,83 @@ const abi = [
 		"inputs": [
 			{
 				"indexed": false,
-				"name": "addr",
-				"type": "address"
+				"name": "ip",
+				"type": "uint32"
 			},
 			{
 				"indexed": false,
-				"name": "id",
+				"name": "name",
 				"type": "string"
 			},
 			{
 				"indexed": false,
 				"name": "status",
 				"type": "string"
-			},
-			{
-				"indexed": false,
-				"name": "dong",
-				"type": "uint32"
-			},
-			{
-				"indexed": false,
-				"name": "ho",
-				"type": "uint32"
 			}
 		],
-		"name": "setCardSuccessEvent",
+		"name": "SetFacility",
 		"type": "event"
 	},
 	{
 		"anonymous": false,
 		"inputs": [
 			{
-				"indexed": false,
+				"indexed": true,
 				"name": "addr",
 				"type": "address"
 			},
 			{
-				"indexed": false,
-				"name": "id",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"name": "status",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"name": "dong",
-				"type": "uint32"
-			},
-			{
-				"indexed": false,
-				"name": "ho",
+				"indexed": true,
+				"name": "ip",
 				"type": "uint32"
 			}
 		],
-		"name": "setCardFailureEvent",
+		"name": "AddCardAuth",
 		"type": "event"
 	},
 	{
 		"anonymous": false,
 		"inputs": [
 			{
-				"indexed": false,
-				"name": "from",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"name": "value",
+				"indexed": true,
+				"name": "timestamp",
 				"type": "uint256"
+			},
+			{
+				"indexed": true,
+				"name": "cardAddr",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "facilityIp",
+				"type": "uint32"
+			},
+			{
+				"indexed": false,
+				"name": "cardId",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "dong",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "ho",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "facilityName",
+				"type": "string"
 			}
 		],
-		"name": "transferEvent",
+		"name": "AccessHistory",
 		"type": "event"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "balanceOf",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "decimals",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint8"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "s1",
-				"type": "string"
-			},
-			{
-				"name": "s2",
-				"type": "string"
-			}
-		],
-		"name": "equals",
-		"outputs": [
-			{
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"payable": false,
-		"stateMutability": "pure",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "name",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "symbol",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "totalSupply",
-		"outputs": [
-			{
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
 	}
 ]
+
+
+export { contractAddr, abi, defaultAccount }
